bcd86e2fb3edd8a0548789db82f8ead9b1412886
lan <lan>
2017-11-21 14:56:54 +0800

    验证成功

--- app/src/main/java/cn/cndoppler/mobsmsandrecorder/MainActivity.java
+++ app/src/main/java/cn/cndoppler/mobsmsandrecorder/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(MainActivity.this, "验证成功", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "验证成功了", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
